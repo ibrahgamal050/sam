aaf9f9c6265e5eced473b5493cca34335635a674
ibrahgamal050 <ibrahimgamal050@gmail.com>
2025-06-22 12:12:35 +0300

    وصف التغييرات

--- src/app/sites/ar/page.tsx
+++ src/app/sites/ar/page.tsx
@@ -1,17 +1,16 @@
+export const runtime = 'nodejs'; // أو 'edge' حسب ما تفضل
+
 import dbConnect from "@/lib/db"
 import Restaurant from "@/models/restaurant"
 import { RestaurantHome } from "@/components/ar/RestaurantHome"
-import { IRestaurant  } from "@/types/restaurant" // Make sure to import the Restaurant type if you have it.
-
-
+import { IRestaurant } from "@/types/restaurant"
 import { headers } from 'next/headers';
 
 export default async function HomeContent() {
   await dbConnect();
 
-  const headersList = await headers(); // ✅ await هنا مهم
-  const host = headersList.get('host'); // ✅ هذا الآن يعمل
-
+  const headersList = await headers(); // ✅ استخدم await
+  const host = headersList.get('host'); // ✅ دلوقتي get هتشتغل
   const subdomain = host?.split('.')[0];
 
   const restaurant = await Restaurant.findOne({ subdomain }).lean() as IRestaurant | null;
