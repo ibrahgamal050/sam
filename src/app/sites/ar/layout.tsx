047005aab6b396c47a5bb46d07f13c44bff91b83
ibrahgamal050 <ibrahimgamal050@gmail.com>
2025-07-02 12:14:30 +0300

    Initial commit

--- src/app/sites/ar/layout.tsx
+++ src/app/sites/ar/layout.tsx
@@ -5,35 +5,32 @@ import { RestaurantProvider } from "@/contexts/restaurant-context"
 import { MobileLayout } from "@/components/ar/mobile-layout"
 import { Footer } from "@/components/ar/footer"
 import Restaurant from "@/models/restaurant"
+import { dbConnect } from "@/lib/db" // ← استورد الدالة
 
 import { IRestaurant } from "@/types/restaurant"
 import { headers } from 'next/headers';
 
 
-export default async function RestaurantLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default async function RestaurantLayout({ children }: { children: React.ReactNode }) {
+  const headersList = await headers()
+  const host = headersList.get("host")
+  const subdomain = host?.split(".")[0]
 
-  const headersList = await headers(); // ✅ استخدم await
-  const host = headersList.get('host'); // ✅ دلوقتي get هتشتغل
-  const subdomain = host?.split('.')[0];
+  await dbConnect() // ✅ الحل هنا
 
-  const restaurant = await Restaurant.findOne({ subdomain }).lean() as IRestaurant | null;
+  const restaurant = await Restaurant.findOne({ subdomain }).lean() as IRestaurant | null
 
   if (!restaurant) {
-    return <div className="p-4">المطعم غير موجود</div>;
+    return <div className="p-4">المطعم غير موجود</div>
   }
+
   return (
     <RestaurantProvider>
-      {/* دي تغلف كل حاجة */}
-      <div className="min-h-screen flex flex-col" dir="rtl" >
+      <div className="min-h-screen flex flex-col" dir="rtl">
         <div className="block lg:hidden">
-          <MobileLayout restaurant={restaurant} >{children}</MobileLayout>
+          <MobileLayout restaurant={restaurant}>{children}</MobileLayout>
         </div>
         <div className="hidden lg:block">
-          {/* نمرر children بنفس الطريقة لكن بدون إعادة تكرار */}
           {children}
         </div>
         <Footer />
